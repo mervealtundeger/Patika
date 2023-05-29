0b6f9a960e480569e55a8f03544c0c67961671e1
MERVE ALTUNDEGER <mervealtundeger56@gmail.com>
2023-05-29 11:55:22 +0300

    Create Taksimetre.java

---
+++ docs/Taksimetre.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double mesafe, tutar;
+        System.out.print("Gidilen mesafe (km): ");
+        mesafe = input.nextDouble();
+        
+        tutar = mesafe * 2.20 + 10.0; // taksimetre açılış ücreti
+        
+        if (tutar < 20.0) {
+            tutar = 20.0; // minimum ödenecek tutar
+        }
+        
+        System.out.printf("Tutar: %.2f TL", tutar);
+        input.close();
+    
+    
+    
+    /*
+    Kullanıcıdan mesafeyi (km cinsinden) alır ve taksimetre tutarını hesaplar. 
+    Taksimetre ücreti km başına 2.20 TL'dir ve açılış ücreti 10 TL'dir. 
+    Minimum ödenecek tutar 20 TL'dir. Eğer hesaplanan tutar 20 TL'den küçükse, yine 20 TL alınır. 
+    Son olarak, hesaplanan tutar ekrana 2 ondalık basamağa kadar yazdırılır.
+    
+    */
+    
+    }
+}
+
