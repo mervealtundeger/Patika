64aae2754319e34a55de114cde1b4a591952ba4d
MERVE ALTUNDEGER <mervealtundeger56@gmail.com>
2023-05-29 09:44:08 +0300

    Update and rename ManavKasa to ManavKasa.java

--- docs/ManavKasa.java
+++ docs/ManavKasa.java
@@ -1,6 +1,17 @@
 
 import java.util.Scanner;
 
+ /*
+Java ile kullanıcıların manavdan almış oldukları ürünlerin kilogram değerlerine göre toplam tutarını ekrana yazdıran programı yazın.
+Meyveler ve KG Fiyatları
+
+Armut : 2,14 TL
+Elma : 3,67 TL
+Domates : 1,11 TL
+Muz: 0,95 TL
+Patlıcan : 5,00 TL
+ */
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
