06e7f8bf6dd1b9bd1c4dc53d0a68ecbdd2f01a77
MERVE ALTUNDEGER <mervealtundeger56@gmail.com>
2023-05-29 14:28:24 +0300

    Update DaireAlanCevre.java

--- docs/DaireAlanCevre.java
+++ docs/DaireAlanCevre.java
@@ -1,3 +1,4 @@
+//Java ile yarı çapını kullanıcıdan aldığınız dairenin alanını ve çevresini hesaplayan program
 import java.util.Scanner;
 
 public class DaireAlanCevre {
@@ -10,8 +11,8 @@ public class DaireAlanCevre {
         System.out.print("Dairenin yarıçapını girin: ");
         double yariCap = input.nextDouble();
 
-        double alan = pi * yariCap * yariCap;
-        double cevre = 2 * pi * yariCap;
+        double alan = pi * yariCap * yariCap; //Alan Formülü : π * r * r;
+        double cevre = 2 * pi * yariCap;      //Çevre Formülü : 2 * π * r;
 
         System.out.println("Dairenin alanı: " + alan);
         System.out.println("Dairenin çevresi: " + cevre);
@@ -19,7 +20,7 @@ public class DaireAlanCevre {
         System.out.print("Daire diliminin merkez açısını girin: ");
         double merkezAci = input.nextDouble();
 
-        double dilimAlan = (pi * yariCap * yariCap * merkezAci) / 360;
+        double dilimAlan = (pi * yariCap * yariCap * merkezAci) / 360; //Formül : (𝜋 * (r*r) * 𝛼) / 360
 
         System.out.println("Daire diliminin alanı: " + dilimAlan);
 
@@ -29,8 +30,7 @@ public class DaireAlanCevre {
       Kullanıcıdan önce dairenin yarıçapını, sonra daire diliminin merkez açısını girmesini istiyor. 
       Daha sonra, önce dairenin alanını ve çevresini hesaplayıp ekrana yazdırıyor, 
       ardından daire diliminin alanını hesaplayıp yine ekrana yazdırıyor.
-      
-      
+        
       */
     }
 }
