6fe753151dcca39a83a84f7c909cdf9451636ec3
MERVE ALTUNDEGER <mervealtundeger56@gmail.com>
2023-05-29 15:11:24 +0300

    Update VucutKitleIndeks.java

--- docs/VucutKitleIndeks.java
+++ docs/VucutKitleIndeks.java
@@ -1,24 +1,22 @@
-import java.util.Scanner;
-
-
-public class Test {
-    public static void main(String[] args) {
-         /*
+  /*
         Kullanıcıdan aldığınız boy ve kilo değerlerine göre kullanıcının beden kitle indeksini bulun.
 
         Beden Kitle İndeksi : Kilo / Boy(m) * Boy(m)
-    
-        */
+  */
+import java.util.Scanner;
+public class Test {
+    public static void main(String[] args) {
+       
          Scanner scanner = new Scanner(System.in);
          
-         System.out.print("Kilonuzu Girin:");
-         int kilo = scanner.nextInt();
-         System.out.print("Boyunuzu Girin:(Örnek :  1,68)");
-         double boy = scanner.nextDouble();
+         System.out.print("Kilonuzu lütfen kg cinsinden girin:"); //Kullanıcıdan kilo(kg) değerini girmesini istiyoruz
+         int kilo = scanner.nextInt(); //bir değişkene atıyoruz
+         System.out.print("Boyunuzu lütefen metre cinsinden girin:"); //Kullanıcıdan boy(m) değerini girmesini istiyoruz
+         double boy = scanner.nextDouble(); //bir değişkene atıyoruz
          
-         double bki = kilo / (boy * boy);
+         double bki = kilo / (boy * boy); //formül--> Kilo (kg) / Boy(m) * Boy(m)
          
-         System.out.println("Beden Kitle Indeksiniz : " + bki);
+         System.out.println("Beden Kitle Indeksiniz : " + bki); //Ekrana bki yazdırıyoruz
          
 
         
