8de173d1217a436c98b08b94c48c4288028a59b1
MERVE ALTUNDEGER <mervealtundeger56@gmail.com>
2023-05-29 14:49:12 +0300

    Update ManavKasa.java

--- docs/ManavKasa.java
+++ docs/ManavKasa.java
@@ -1,6 +1,3 @@
-
-import java.util.Scanner;
-
  /*
 Java ile kullanıcıların manavdan almış oldukları ürünlerin kilogram değerlerine göre toplam tutarını ekrana yazdıran programı yazın.
 Meyveler ve KG Fiyatları
@@ -11,17 +8,18 @@ Domates : 1,11 TL
 Muz: 0,95 TL
 Patlıcan : 5,00 TL
  */
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
+        Scanner scanner = new Scanner(System.in); //İlk olarak, Scanner sınıfı kullanılarak kullanıcıdan alınacak verileri almak için bir nesne oluşturdum
+        //Sonra, her bir ürün için kilogram başına fiyatları değişkenlere atadım
         double armutFiyati = 2.14;
         double elmaFiyati = 3.67;
         double domatesFiyati = 1.11;
         double muzFiyati = 0.95;
         double patlicanFiyati = 5.00;
-
+        //Kullanıcıdan kaç kilo alınacak diye sorulur ve kullanıcı tarafından girilen değerler değişkenlere atanır
         System.out.print("Kaç kilo armut aldınız? ");
         double armutKilosu = scanner.nextDouble();
         System.out.print("Kaç kilo elma aldınız? ");
@@ -32,13 +30,13 @@ public class Main {
         double muzKilosu = scanner.nextDouble();
         System.out.print("Kaç kilo patlıcan aldınız? ");
         double patlicanKilosu = scanner.nextDouble();
-
+        //En son olarak, toplam tutar hesaplanır
         double toplamTutar = armutFiyati * armutKilosu + 
                             elmaFiyati * elmaKilosu +
                             domatesFiyati * domatesKilosu +
                             muzFiyati * muzKilosu +
                             patlicanFiyati * patlicanKilosu;
-
+       //ekrana yazdırılır 
         System.out.printf("Toplam tutar: %.2f TL", toplamTutar);
     }
 }
