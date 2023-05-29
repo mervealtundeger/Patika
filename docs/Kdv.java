a535db2ed1c958e45dcb3f10fde9be82554b9e02
MERVE ALTUNDEGER <mervealtundeger56@gmail.com>
2023-05-29 14:45:56 +0300

    Update Kdv.java

--- docs/Kdv.java
+++ docs/Kdv.java
@@ -1,29 +1,29 @@
+//Java ile kullanıcıdan alınan para değerinin KDV'li fiyatını ve KDV tutarını hesaplayıp ekrana bastıran programı
 import java.util.Scanner;
 
 public class KdvHesaplama {
 
     public static void main(String[] args) {
         double girilenTutar, kdvOrani, kdvTutari, kdvliFiyat, kdvsizFiyat;
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); //Scanner sınıfı kullanarak kullanıcıdan bir fiyat alınır. 
         System.out.print("Lütfen ürünün fiyatını giriniz: ");
         girilenTutar = scanner.nextDouble();
         
-        if(girilenTutar > 0 && girilenTutar <= 1000) {
-            kdvOrani = 0.18;
+        if(girilenTutar > 0 && girilenTutar <= 1000) { //girilen fiyat 0'dan büyük ve eşit mi, 1000'den küçük veya eşit mi, yoksa 1000'den büyük mü diye kontrol edilir
+            kdvOrani = 0.18; //Bu kontrollere göre kdvOrani belirlenir
         } else {
             kdvOrani = 0.08;
         }
-        
+        //kdvTutari değişkenleri belirlenir ve hesaplanır
         kdvsizFiyat = girilenTutar;
         kdvTutari = kdvsizFiyat * kdvOrani;
         kdvliFiyat = kdvsizFiyat + kdvTutari;
-        
+        //print edilir
         System.out.println("KDV'siz fiyat: " + kdvsizFiyat);
         System.out.println("KDV oranı: " + (kdvOrani*100) + "%");
         System.out.println("KDV tutarı: " + kdvTutari);
         System.out.println("KDV'li fiyat: " + kdvliFiyat);
     /*
-    Java ile kullanıcıdan alınan para değerinin KDV'li fiyatını ve KDV tutarını hesaplayıp ekrana bastıran program.  
     (Not : KDV tutarını 18% olarak alındı)  
     KDV'siz Fiyat = 10;  
     KDV'li Fiyat = 11.8;  
