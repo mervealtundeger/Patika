d240e415a29e492954f6ffe2c9727d4bb35f2465
MERVE ALTUNDEGER <mervealtundeger56@gmail.com>
2023-05-29 15:06:26 +0300

    Update UcakBiletFiyat.java

--- docs/UcakBiletFiyat.java
+++ docs/UcakBiletFiyat.java
@@ -1,9 +1,6 @@
-import java.util.Scanner;
-
-    /*
-    Bu program, kullanıcıdan mesafe, yaş ve yolculuk tipi bilgilerini alacak ve aldığı bu bilgilere göre uçak biletinin toplam ücretini hesaplayacak. 
-    Buna ek olarak aşağıdaki indirimleri uygulayacak:
-
+/*
+ Bu program, kullanıcıdan mesafe, yaş ve yolculuk tipi bilgilerini alacak ve aldığı bu bilgilere göre uçak biletinin toplam ücretini hesaplayacak. 
+ Buna ek olarak aşağıdaki indirimleri uygulayacak:
 - Kullanıcının yaş değeri 12'den küçükse, bilet fiyatı üzerinden %50 indirimi uygulanacak.
 - Kullanıcının yaş değeri 12-24 arasında ise, bilet fiyatı üzerinden %10 indirimi uygulanacak.
 - Kullanıcının yaş değeri 65'ten büyükse, bilet fiyatı üzerinden %30 indirim uygulanacak.
@@ -14,9 +11,8 @@ Mesafe değeri girildikten sonra, program mesafeyi 0,10 TL / km oranında çarpa
 Sonrasında, kullanıcının yaş değeri kontrol edilecek ve ilgili indirimler bilet fiyatına uygulanacaktır. 
 Yolculuk tipi de kontrol edilecek ve ilgili indirim bu bilgiye göre uygulanacaktır.
 Eğer kullanıcının girdiği veri doğru değilse, program kullanıcıya bir uyarı mesajı verecek ve programı yeniden başlatması gerekecek.
-    
-    
-    */
+*/
+import java.util.Scanner;
 public class UcakBiletiHesaplama {
 
     public static void main(String[] args) {
@@ -25,30 +21,31 @@ public class UcakBiletiHesaplama {
         double mesafe, ucret, toplamFiyat, indirimliFiyat;
         int yas, yolculukTipi;
 
-        System.out.print("Lütfen mesafeyi (km) giriniz: ");
+        System.out.print("Lütfen mesafeyi (km) giriniz: "); //Kullanıcıdan mesafe girşi yapmasını istiyoruz 
         mesafe = scanner.nextDouble();
-        if (mesafe <= 0) {
+        if (mesafe <= 0) { //Girilen değer 0'a eşite veya küçükse "Hatalı veri girdiniz!" mesajını ekrana yazdırıyoruz
             System.out.println("Hatalı veri girdiniz!");
             return;
         }
-
+        //Mesafe değeri girildikten sonra, program mesafeyi 0,10 TL / km oranında çarpacak ve toplam fiyatı hesaplayacaktır.
         ucret = mesafe * 0.1;
         toplamFiyat = ucret;
-
-        System.out.print("Lütfen yaşınızı giriniz: ");
+        //Başlamak için, ilk olarak kullanıcıdan mesafe, yaş ve yolculuk tipi bilgilerini istiyoruz
+        System.out.print("Lütfen yaşınızı giriniz: "); 
         yas = scanner.nextInt();
-        if (yas < 0) {
+        if (yas < 0) {  //Girilen değer 0'a eşite veya küçükse "Hatalı veri girdiniz!" mesajını ekrana yazdırıyoruz
             System.out.println("Hatalı veri girdiniz!");
             return;
         }
 
         System.out.print("Lütfen yolculuk tipini seçiniz (1 - Tek Yön, 2 - Gidiş Dönüş): ");
         yolculukTipi = scanner.nextInt();
-        if (yolculukTipi != 1 && yolculukTipi != 2) {
+        //Eğer kullanıcının girdiği veri doğru değilse, program kullanıcıya bir uyarı mesajı verecek ve programı yeniden başlatması gerekecek.
+        if (yolculukTipi != 1 && yolculukTipi != 2) { 
             System.out.println("Hatalı veri girdiniz!");
             return;
         }
-
+          //kullanıcının yaş değeri kontrol edilecek ve ilgili indirimler bilet fiyatına uygulanacaktır.
         if (yas < 12) {
             indirimliFiyat = toplamFiyat * 0.5;
             toplamFiyat -= indirimliFiyat;
@@ -59,12 +56,12 @@ public class UcakBiletiHesaplama {
             indirimliFiyat = toplamFiyat * 0;
             toplamFiyat -= indirimliFiyat;
         }
-
+           //Yolculuk tipi de kontrol edilecek ve ilgili indirim bu bilgiye göre uygulanacaktır.
         if (yolculukTipi == 2) {
             indirimliFiyat = toplamFiyat * 0.2;
             toplamFiyat -= indirimliFiyat;
         }
-
+        //Hesaplanan değer "toplamfiyat" ekrana yazdırılır
         System.out.println("Toplam fiyat: " + toplamFiyat + " TL");
     }
 }
