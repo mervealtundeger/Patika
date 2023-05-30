0cddac1f00f7be7d096361f772b5c50875bcfca7
MERVE ALTUNDEGER <mervealtundeger56@gmail.com>
2023-05-30 15:20:04 +0300

    Update Main.java

--- MayinTarlasi/Main.java
+++ MayinTarlasi/Main.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Satir sayisini giriniz: ");
+        int row = scanner.nextInt();
+        System.out.print("Sutun sayisini giriniz: ");
+        int col = scanner.nextInt();
+
+        Minesweeper game = new Minesweeper(row, col);
+        game.play();
+    }
+}
+
 
