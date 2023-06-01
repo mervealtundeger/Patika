f91a440f9aa3dc07eb365864cbff244f5464f1f6
MERVE ALTUNDEGER <mervealtundeger56@gmail.com>
2023-06-01 11:37:00 +0300

    Update Main.java

--- MayinTarlasi/Main.java
+++ MayinTarlasi/Main.java
@@ -13,21 +13,5 @@ public class Main {  // Main adında bir sınıf oluşturduk, bu sınıfın eri�
         game.play(); // Minesweeper nesnesindeki play() metodunu çağrdık ve oyunu başlattık
     }
 }
-/*
 
-Oyun Kuralları :
-Oyun metin tabanlıdır.
-Çift boyutlu diziler üzerinden oynanmalı ve MineSweeper sınıfı içerisinde tasarlanmalı.
-Matris boyutunu yani satır ve sütun sayısını kullanıcı belirlemeli.
-Diziye ait eleman sayısının çeyreği (elemanSayisi / 4) kadar rastgele mayın yerleştirilmeli. 
-Örneğin dizi 4x3 boyutunda ise eleman sayısı (satırSayısı * sütunSayısı) formülü ile hesaplanmalı ve boyutu 12 olmalı. 
-Bu durumda mayın sayısı 12 / 4 = 3 adet olmalıdır. (ipucu : bu mayınların konumlarını tutacak ikinci bir dizi oluşturabilirsiniz.)
-Kullanıcı matris üzerinden bir nokta seçmeli. Nokta seçimi için satır ve sütun değerlerini girmeli.
-Seçilen noktanın dizinin sınırları içerisinde olup olmadığını kontrol edilmeli ve koşul sağlanmazsa tekrar nokta istenmeli.
-Kullanıcının girdiği noktada mayın var ise oyunu kaybetmeli.
-Mayın yok ise, ilgili noktaya değen tüm konumlarına bakılmalı (sağı, solu, yukarısı, aşağısı, sol üst çapraz, sağ üst çapraz, sağ alt çapraz, sol alt çapraz) 
-ve etrafındaki mayınların sayısının toplamı ilgili noktaya yazılmalı. Noktaya değen herhangi bir mayın yok ise "0" değeri atanmalı.
-Kullanıcı hiç bir mayına basmadan tüm noktaları seçebilirse oyunu kazanmalı.
-
-*/
 
